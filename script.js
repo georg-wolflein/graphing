b04547081872898a3bb1fdf03a49a954050e3e40
Munchbot <david_charatan@live.com>
2014-06-30 05:03:07 -0700

    Add some basic stuff
    Added a container element and a function to draw stuff (no content)

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    drawBarGraph(
+        [[34, "Type 1"], [19, "Type 2"], [24, "Type 3"], [36, "Type 4"], [8, "Type 5"]],
+        ["#00CC66"],
+        "Graph Title",
+        "Graph Subtitle");
+});
+
+function drawBarGraph(dataArray, colorArray, graphTitle, graphSubtitle) {
+    
+}
